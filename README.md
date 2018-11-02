77c0e9d3fb9e42d9960f835c3d8a327c4980a7d0
LDericher <40151420+ldericher@users.noreply.github.com>
2018-11-02 23:13:17 +0100

    Format strings, CLI interface, Single request to ffdecks.com basic API instead of per-card requests

--- README.md
+++ README.md
@@ -3,26 +3,34 @@
 Card import tool for [Final Fantasy TCG Complete](https://steamcommunity.com/sharedfiles/filedetails/?id=889160751) mod for the [Tabletop Simulator](http://berserk-games.com/tabletop-simulator/)
 
 
-## Quick Start
+## Usage
 
-If needed, customize `main.py` variables:
+    usage: main.py [-h] [-n COUNT] [OpusID]
 
-- `opusid`: The Card opus you want to import
-- `num_threads`: Lower this for really weak systems
-- `opus_size`: 500 here means the script will correctly import anything containing *up to* 500 cards
+    Imports FFTCG cards for TT-Sim.
 
-### using your system's `python3`
+    positional arguments:
+      OpusID                the Opus to import
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -n COUNT, --num_threads COUNT
+                            maximum number of concurrent requests
+
+### Run using your system's `python3`
 
 1. Make sure `PIL` and `requests` python3 libraries (or equivalent) are installed.
-1. Run `main.py` in project root directory.
+1. Run `./main.py` from project root directory.
 
-### using a `docker` container
+### Run using a `docker` container
 
-1. Build the container using `docker build -t ldericher/fftcgtool .` in project root directory.
-1. Run `docker run --rm -it -v "$(pwd)/out:/app/out" ldericher/fftcgtool` in project root directory.
+1. Make sure you have a working installation of `docker` software.
+1. Update your local image `docker pull ldericher/fftcgtool`.
+1. Run `docker run --rm -it -v "$(pwd)/out:/app/out" ldericher/fftcgtool` in any directory.
+
+Output files will go to subdirectory `./out`. CLI arguments are supported as `docker run --rm -it -v "$(pwd)/out:/app/out" ldericher/fftcgtool -n 2 5` (imports Opus 5 using 2 threads)
 
 
 ## Future work
 
-- Add command line interface for `main.py` variables
 - Multiple opus import
--- main.py
+++ main.py
@@ -1,59 +1,90 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-# base config
-opusid = 7 # opus prefix
-num_threads = 8 # maximum concurrent requests
-opus_size = 500 # maximum card count per opus
+import argparse
+import json
+import logging
+import os
+import requests
+
+from opus import Opus
 
 # constants
-grid = 7, 10    # default in TTsim: 7 rows, 10 columns
-reso = 429, 600 # default in TTsim: 480x670 pixels per card
+GRID = 7, 10    # default in TTsim: 7 rows, 10 columns
+RESO = 429, 600 # default in TTsim: 480x670 pixels per card
+FURL = "https://ffdecks.com/api/cards/basic" # FFDecks API URL
 
-# set up logging
-import logging
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(threadName)s %(message)s')
-logger = logging.getLogger(__name__)
+def main():
+    # Setup CLI
+    parser = argparse.ArgumentParser(
+        description='Imports FFTCG cards for TT-Sim.')
 
-# output directory
-import os
-if not os.path.exists("out"):
-    os.mkdir("out")
-os.chdir("out")
+    parser.add_argument(
+        'opusid',
+        default="7",
+        metavar="OpusID",
+        nargs="?",
+        help='the Opus to import')
 
-# load an Opus
-from opus import Opus
-myOpus = Opus()
-myOpus.load(opusid, opus_size, num_threads)
+    parser.add_argument(
+        '-n', '--num_threads',
+        type=int,
+        default=20,
+        metavar="COUNT",
+        help='maximum number of concurrent requests')
 
-# compose custom deck images
-faceurls = []
-for i, image in enumerate(myOpus.get_images(grid, reso, num_threads)):
-    filename = "opus_%d_%d.jpg" % (opusid, i)
-    image.save(filename)
-    # ask for upload
-    iurl = input("Upload '%s' and paste URL: " % (filename))
-    if not iurl:
-        # add local file (maybe upload to steam cloud in cloud manager)
-        logging.warn("Using local file for '%s'." % (filename))
-        iurl = "file://" + os.path.abspath(filename)
-    faceurls.append(iurl)
+    args = parser.parse_args()
 
-# Build json for element decks
-elementaldecks = [
-    ["Fire"],
-    ["Water"],
-    ["Lightning"],
-    ["Ice"],
-    ["Wind"],
-    ["Earth"],
-    ["Light", "Dark"]
-]
-for i, elements in enumerate(elementaldecks):
-    json_filename = "opus_%d_%s.json" % (opusid, "_".join(elements))
-    with open(json_filename, "w") as json_file:
-        cardfilter = lambda card: card._element in elements
-        json_data = myOpus.get_json("/".join(elements), grid, cardfilter, faceurls)
-        json_file.write(json_data)
+    # Setup logging
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(threadName)s %(message)s')
+    logger = logging.getLogger(__name__)
 
-logging.info("Done. Put the generated JSON files in your 'Saved Objects' Folder.")
-logging.info("Thanks for using fftcgtool!")
+    # Fetch and parse card database from ffdecks API
+    ffdecks_raw = requests.get(FURL)
+    ffdecks = json.loads(ffdecks_raw.content.decode("utf-8"))
+
+    # Load an Opus
+    opus_data = (card_data for card_data in ffdecks["cards"] if card_data["serial_number"].startswith(args.opusid))
+    myOpus = Opus(opus_data)
+
+    # output directory
+    if not os.path.exists("out"):
+        os.mkdir("out")
+    os.chdir("out")
+
+    # compose custom deck images
+    faceurls = []
+    for i, image in enumerate(myOpus.get_images(GRID, RESO, args.num_threads)):
+        filename = "opus_{}_{}.jpg".format(args.opusid, i)
+        image.save(filename)
+        # ask for upload
+        iurl = input("Upload '{}' and paste URL: ".format(filename))
+        if not iurl:
+            # add local file (maybe upload to steam cloud in cloud manager)
+            logging.warn("Using local file for '{}'.".format(filename))
+            iurl = "file://" + os.path.abspath(filename)
+        faceurls.append(iurl)
+
+    # Build json for element decks
+    elementaldecks = [
+        ["Fire"],
+        ["Water"],
+        ["Lightning"],
+        ["Ice"],
+        ["Wind"],
+        ["Earth"],
+        ["Light", "Dark"]
+    ]
+    for i, elements in enumerate(elementaldecks):
+        json_filename = "opus_{}_{}.json".format(args.opusid, "_".join(elements))
+        with open(json_filename, "w") as json_file:
+            cardfilter = lambda card: card._element in elements
+            json_data = myOpus.get_json(args.opusid, "/".join(elements), GRID, cardfilter, faceurls)
+            json_file.write(json_data)
+
+    # Bye
+    logging.info("Done. Put the generated JSON files in your 'Saved Objects' Folder.")
+    logging.info("Thanks for using fftcgtool!")
+
+
+if __name__ == "__main__":
+   main()
