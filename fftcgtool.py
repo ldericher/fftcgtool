92d8283e39f309187cd893ad094c3dd343171b75
LDericher <40151420+ldericher@users.noreply.github.com>
2021-09-03 11:52:05 +0200

    argparse -> click

--- fftcgtool.py
+++ fftcgtool.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 import logging
 import os
 import sys
 import zipfile
+
+import click
 
 import fftcg
 
@@ -11,132 +12,61 @@ import fftcg
 OUT_DIR_NAME = "out"  # name of output directory
 
 
-def opus_decks(args: argparse.Namespace) -> list[fftcg.TTSDeck]:
-    carddb = fftcg.CardDB()
-    decks: list[fftcg.TTSDeck] = []
-    for opus_id in args.opus_ids:
-        # import an opus
-        opus = fftcg.Opus(opus_id, args.language)
-        book = fftcg.Book(opus, args.language, args.num_requests)
-        book.save()
+class LanguageParamType(click.ParamType):
+    name = "lang"
 
-        carddb.update(opus)
-        decks.extend(opus.elemental_decks)
-
-    carddb.upload_prompt()
-
-    # create elemental decks for opus
-    return decks
+    def convert(self, value, param, ctx) -> fftcg.Language:
+        if isinstance(value, fftcg.Language):
+            return value
+        elif isinstance(value, str):
+            return fftcg.Language(value)
+        else:
+            return fftcg.Language("")
 
 
-def ffdecks_decks(args: argparse.Namespace) -> list[fftcg.TTSDeck]:
-    decks: list[fftcg.TTSDeck] = []
-    for deck_id in args.deck_ids:
-        # import a deck
-        decks.append(fftcg.TTSDeck.from_ffdecks_deck(deck_id))
-
-    return decks
+LANGUAGE = LanguageParamType()
 
 
-def main() -> None:
-    # set up CLI
+@click.group()
+@click.option(
+    "-v", "--verbose",
+    help="increase output verbosity",
+    count=True,
+)
+@click.option(
+    "-l", "--language",
+    type=LANGUAGE,
+    default="en",
+    help="language for imported objects",
+)
+@click.option(
+    "-s", "--stdout",
+    is_flag=True,
+    help="print the deck files in a zip archive to stdout, skip creating JSONs on disk",
+)
+@click.pass_context
+def main(ctx, verbose, language, stdout) -> None:
+    """Imports FFTCG cards for TT-Sim."""
 
-    # main parser
-    parser = argparse.ArgumentParser(
-        prog="fftcgtool",
-        description="Imports FFTCG cards for TT-Sim.",
-    )
-
-    parser.add_argument(
-        "-v", "--verbose",
-        help="increase output verbosity",
-        action="count",
-    )
-
-    parser.add_argument(
-        "-l", "--language",
-        type=fftcg.Language,
-        default="en",
-        metavar="LANG",
-        help="language for imported objects",
-    )
-
-    parser.add_argument(
-        "-s", "--stdout",
-        action="store_true",
-        help="print the deck files in a zip archive to stdout, skip creating JSONs on disk",
-    )
-
-    subparsers = parser.add_subparsers(
-        description="Import either an Opus to extend the mod, or import a deck to play right away.",
-        dest="subcommand",
-        help="valid subcommands",
-        required=True,
-    )
-
-    # "opus" subcommand
-    opuses_parser = subparsers.add_parser(
-        "opuses",
-        description="Imports Opuses from the square API and creates its elemental decks as JSON files.",
-    )
-
-    opuses_parser.set_defaults(
-        func=opus_decks
-    )
-
-    opuses_parser.add_argument(
-        "opus_ids",
-        type=str,
-        nargs="+",
-        metavar="Opus_ID",
-        help="the Opuses to import",
-    )
-
-    opuses_parser.add_argument(
-        "-n", "--num_requests",
-        type=int,
-        default=20,
-        metavar="COUNT",
-        help="maximum number of concurrent requests",
-    )
-
-    # "ffdecks" subcommand
-    ffdecks_parser = subparsers.add_parser(
-        "ffdecks",
-        description="Imports Decks from the ffdecks.com API and creates it as a JSON file.",
-    )
-
-    ffdecks_parser.set_defaults(
-        func=ffdecks_decks
-    )
-
-    ffdecks_parser.add_argument(
-        "deck_ids",
-        type=str,
-        nargs="+",
-        metavar="Deck_ID",
-        help="the Decks to import",
-    )
-
-    # parse arguments
-    args = parser.parse_args()
+    ctx.ensure_object(dict)
+    ctx.obj['LANG'] = language
 
     # set up logging
-    if args.verbose is None:
-        args.verbose = logging.WARN
-    elif args.verbose == 1:
-        args.verbose = logging.INFO
+    if verbose == 0:
+        verbose = logging.WARN
+    elif verbose == 1:
+        verbose = logging.INFO
     else:
-        args.verbose = logging.DEBUG
+        verbose = logging.DEBUG
 
     logging.basicConfig(
-        level=args.verbose,
+        level=verbose,
         format="%(levelname)s: %(processName)s %(message)s",
     )
 
     logger = logging.getLogger(__name__)
     logger.info("fftcgtool started.")
-    logger.debug(f"{args = }")
+    logger.debug(f"args: {verbose = }, {language = }, {stdout = }")
 
     # output directory
     if not os.path.exists(OUT_DIR_NAME):
@@ -148,22 +78,89 @@ def main() -> None:
     carddb = fftcg.CardDB()
     carddb.load()
 
-    # call function based on args
-    decks = args.func(args)
+
+@main.command()
+@click.option(
+    "-n", "--num_requests",
+    type=int,
+    default=20,
+    help="maximum number of concurrent requests",
+)
+@click.argument(
+    "opus_ids",
+    nargs=-1,
+    type=str,
+    metavar="[OPUS_ID] ...",
+)
+@click.pass_context
+def opuses(ctx, opus_ids, num_requests) -> list[fftcg.TTSDeck]:
+    """
+    Imports Opuses from the square API and creates its elemental decks as JSON files.
+
+    OPUS_ID: each of the Opuses to import
+    """
+
+    ctx.ensure_object(dict)
+    language = ctx.obj['LANG'] or fftcg.Language("")
+
+    carddb = fftcg.CardDB()
+    decks: list[fftcg.TTSDeck] = []
+    for opus_id in opus_ids:
+        # import an opus
+        opus = fftcg.Opus(opus_id, language)
+        book = fftcg.Book(opus, language, num_requests)
+        book.save()
+
+        carddb.update(opus)
+        decks.extend(opus.elemental_decks)
+
+    carddb.upload_prompt()
+
+    # create elemental decks for opus
+    return decks
+
+
+@main.command()
+@click.argument(
+    "deck_ids",
+    nargs=-1,
+    type=str,
+    metavar="[DECK_ID] ...",
+)
+def ffdecks(deck_ids) -> list[fftcg.TTSDeck]:
+    """
+    Imports Decks from the ffdecks.com API and creates it as a JSON file.
+
+    DECK_ID: each of the Decks to import
+    """
+
+    print(f"{deck_ids = }")
+    decks: list[fftcg.TTSDeck] = []
+    for deck_id in deck_ids:
+        # import a deck
+        decks.append(fftcg.TTSDeck.from_ffdecks_deck(deck_id))
+
+    return decks
+
+
+@main.result_callback()
+def process_decks(decks: list[fftcg.TTSDeck], verbose, language, stdout):
+    # arg needed because it's in this group
+    int(verbose)
 
     # decide what to do with the decks
-    if args.stdout:
+    if stdout:
         # print out a zip file
         with open(sys.stdout.fileno(), "wb", closefd=False, buffering=0) as raw_stdout:
             with zipfile.ZipFile(raw_stdout, "w", compression=zipfile.ZIP_DEFLATED) as zip_file:
                 # put the decks into that zip file
                 for deck in decks:
-                    zip_file.writestr(deck.file_name, deck.get_json(args.language))
+                    zip_file.writestr(deck.file_name, deck.get_json(language))
 
     else:
         # save the decks to disk
         for deck in decks:
-            deck.save(args.language)
+            deck.save(language)
 
         # bye
         print("Done. Put the generated JSON files in your 'Saved Objects' Folder.")
